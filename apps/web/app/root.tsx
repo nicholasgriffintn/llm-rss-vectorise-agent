5d61d0a5ec975abb0eac038777edef745e9dd21d
Nicholas Griffin <nicholas.griffin@bbc.co.uk>
2024-08-25 03:22:03 +0100

    feat: adding footer

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -31,6 +31,27 @@ export function Layout({ children }: { children: React.ReactNode }) {
             </div>
           </header>
           <main>{children}</main>
+          <footer className="w-full py-4 px-4 sm:px-6 lg:px-8">
+            <div className="w-full max-w-3xl py-4 px-4 sm:px-6 lg:px-8 m-auto items-center text-center">
+              <p className="text-muted-foreground text-xs text-center">
+                Please note that this application was created for research
+                purposes only, it is not the intention to cause any negative
+                affects to the sites that the systems store or display. If you
+                would like your site removed from the system, please contact me.
+              </p>
+            </div>
+            <div className="flex items-center justify-center gap-2">
+              <span className="text-muted-foreground text-xs">
+                Created by{' '}
+                <a href="https://nicholasgriffin.dev">Nicholas Griffin</a>
+              </span>
+              <span className="text-muted-foreground text-xs">•</span>
+              <span className="text-muted-foreground text-xs">
+                Powered by <a href="https://cloudflare.com">Cloudflare</a> and{' '}
+                <a href="https://remix.run">Remix</a>
+              </span>
+            </div>
+          </footer>
         </div>
         <ScrollRestoration />
         <Scripts />
